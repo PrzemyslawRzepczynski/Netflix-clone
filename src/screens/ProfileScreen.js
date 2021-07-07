786b780d243f1061899adf948e04177f0ae76af0
Przemyslaw Rzepczynski <przemek.rzepczynski94@gmail.com>
2021-07-07 16:25:23 +0200

    com

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
 import Nav from "../Nav";
+import PlansScreen from "./PlansScreen";
 
 import "./ProfileScreen.css";
 
@@ -23,7 +24,7 @@ function ProfileScreen() {
             <h2>{user.email}</h2>
             <div className="profileScreen_plans">
               <h3>Plans</h3>
-              <h5>Deploy</h5>
+              <PlansScreen />
               <button
                 onClick={() => auth.signOut()}
                 className="profileScreen_signOut"
